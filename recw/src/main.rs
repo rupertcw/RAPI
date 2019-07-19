980f4fad8f153d2875ee00306e0d49af7b8c97b6
rupertcw <rupertcw10@gmail.com>
2019-07-19 12:16:05 +0100

    Created 3 get routes - one with a pre-defined handler. Shows Json serialisation example.

--- recw/src/main.rs
+++ recw/src/main.rs
@@ -1,16 +1,36 @@
+extern crate rustc_serialize;
 #[macro_use] extern crate nickel;
 
-use nickel::{Nickel, HttpRouter};
+use nickel::{Nickel, HttpRouter, Request, Response, MediaType, MiddlewareResult};
+use nickel::hyper::method::Method;
+
+use rustc_serialize::json;
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Person {
+    firstname: String,
+    lastname:  String,
+}
+
+fn get_person<'r, 'mw, 'conn>(_: &mut Request<'mw, 'conn>, response: Response<'mw>) -> MiddlewareResult<'mw> {
+    let person = Person{
+        firstname: String::from("Bob"),
+        lastname: String::from("Dylan"),
+    };
+    let json = json::encode(&person).unwrap();
+    response.send(json)
+}
+
+fn hello(word: &str) -> String
+{
+    String::from(format!("Hello {}", word))
+}
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.get("/bar", middleware!("This is the /bar handler"));
-    server.get("/user/:userid", middleware! { |request|
-        format!("This is user: {:?}", request.param("userid"))
-    });
-    server.get("/a/*/d", middleware!("matches /a/b/d but not /a/b/c/d"));
-    server.get("/a/**/d", middleware!("This matches /a/b/d and also /a/b/c/d"));
-
+    server.get("/", middleware!({format!("{}", hello("index"))}));
+    server.get("/hello", middleware!({format!("{}", hello("world"))}));
+    server.add_route(Method::Get, "/person", get_person);
     server.listen("127.0.0.1:6767");
 }
